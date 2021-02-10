1417779d24ca6327e5cdce32914df29e0a824d1b
Jewell Myers <64042050+ToddJewellMyers@users.noreply.github.com>
2021-02-09 21:54:37 -0500

    Revision

--- Monster_Mash/Assets/Scripts/Player_movement.gd
+++ Monster_Mash/Assets/Scripts/Player_movement.gd
@@ -4,46 +4,22 @@ extends KinematicBody2D
 
 var candy = preload("res://Assets/Scenes/Candy.tscn")
 
-func _process(delta):
-	
-	SkillLoop()
-
-
-func SkillLoop(): 
-	var shoot = Vector2()
-	var CANDY_SPEED = 450
-	if Input.get_action_strength("Shoot_right"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)
-		
-		shoot = shoot.normalized() * CANDY_SPEED
-	if Input.get_action_strength("Shoot_left"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)
-		
-		shoot = shoot.normalized() * CANDY_SPEED
-	if Input.get_action_strength("Shoot_down"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)
-		
-		shoot = shoot.normalized() * CANDY_SPEED
-	if Input.get_action_strength("Shoot_up"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)
-		
-		shoot = shoot.normalized() * CANDY_SPEED
-							
-
-const MOTION_SPEED = 400 # Pixels/second.
- 
-
+const DIRECTIONS = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
+const ARROW_KEYS = ["Shoot_up", "Shoot_down", "Shoot_left", "Shoot_right"]						
+ # Pixels/second.
 func _physics_process(_delta):
+	var MOTION_SPEED = 400
 	var motion = Vector2()
+	for i in range(4):
+		if Input.is_action_just_pressed(ARROW_KEYS[i]):
+			var candy_start = get_node(".").position
+			var candy_instance = candy.instance()
+			candy_instance.position = candy_start
+			get_parent().get_parent().add_child(candy_instance)
+			candy_instance.velocity = DIRECTIONS[i] * candy_instance.MOTION_SPEED
 	
 	motion.x = Input.get_action_strength("Move_right") - Input.get_action_strength("Move_left")
 	motion.y = Input.get_action_strength("Move_down") - Input.get_action_strength("Move_up")
-	#shoot.x = Input.get_action_strength("Shoot_right") - Input.get_action_strength("Shoot_left")
-	#shoot.y = Input.get_action_strength("Shoot_down") - Input.get_action_strength("Shoot_up")
 	motion.y *= 0.5
 	motion = motion.normalized() * MOTION_SPEED
 	#warning-ignore:return_value_discarded
