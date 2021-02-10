d30623b69dcb2694c76421e22f6275642ffb07d8
Jewell Myers <64042050+ToddJewellMyers@users.noreply.github.com>
2021-02-09 19:48:48 -0500

    Revision

--- Monster_Mash/Assets/Scripts/EnemySpawner.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-
-var enemy_scene = load("res://Scenes/Enemy.tscn")
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	var enemy = enemy_scene.instance()
-	add_child(enemy)
