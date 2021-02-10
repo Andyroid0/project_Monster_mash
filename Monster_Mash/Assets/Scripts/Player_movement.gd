6988c4144b5fcd7f880adaf08de7015d36f93688
Andrew Valley <andyvalley@live.com>
2021-02-09 22:11:55 -0500

    Revision

--- Monster_Mash/Assets/Scripts/Player_movement.gd
+++ Monster_Mash/Assets/Scripts/Player_movement.gd
@@ -4,46 +4,12 @@ extends KinematicBody2D
 
 var candy = preload("res://Assets/Scenes/Candy.tscn")
 
-<<<<<<< HEAD
-func _process(delta):
-	
-	SkillLoop()
 
-
-func SkillLoop(): 
-	var shoot = Vector2()
-	var CANDY_SPEED = 450
-	if Input.get_action_strength("Shoot_right"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)		
-		shoot = shoot.normalized() * CANDY_SPEED
-		
-	if Input.get_action_strength("Shoot_left"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)		
-		shoot = shoot.normalized() * CANDY_SPEED
-		
-	if Input.get_action_strength("Shoot_down"):
-		var candy_instance = candy.instance()
-		get_parent().add_child(candy_instance)		
-		shoot = shoot.normalized() * CANDY_SPEED
-		
-	if Input.is_action_just_pressed("Shoot_up"):
-		var candy_instance = candy.instance()
-		var candy_start = get_node(".").position
-		candy_instance.position = candy_start
-		get_parent().get_parent().add_child(candy_instance)			
-		shoot.y = shoot.y * CANDY_SPEED
-							
-
-const MOTION_SPEED = 400 # Pixels/second.
  
-
-=======
 const DIRECTIONS = [Vector2.UP, Vector2.DOWN, Vector2.LEFT, Vector2.RIGHT]
 const ARROW_KEYS = ["Shoot_up", "Shoot_down", "Shoot_left", "Shoot_right"]						
  # Pixels/second.
->>>>>>> 063c48dc87a73c9fca463b138810d9f2d03c9108
+
 func _physics_process(_delta):
 	var MOTION_SPEED = 400
 	var motion = Vector2()
