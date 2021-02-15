17c8f187d0e2eb20a21cfcc3a59c0ece1cb1c645
Andrew Valley <andyvalley@live.com>
2021-02-15 18:56:20 -0500

    Revision

--- Monster_Mash/Assets/Scripts/Player/Player_shooting.gd
+++ Monster_Mash/Assets/Scripts/Player/Player_shooting.gd
@@ -9,7 +9,7 @@ func _process(_delta):
 	for i in range(4):
 		if Input.is_action_just_pressed(ARROW_KEYS[i]):
 			var candy_instance = candy.instance()
-			candy_instance.position = get_global_position() + DIRECTIONS[i] * 50
+			candy_instance.position = get_global_position() + DIRECTIONS[i] * 80
 			get_parent().add_child(candy_instance)
 			candy_instance.apply_impulse( Vector2(), DIRECTIONS[i] * CANDY_SPEED )
 			
--- Monster_Mash/Assets/Scripts/Player/candySystem.gd
+++ Monster_Mash/Assets/Scripts/Player/candySystem.gd
@@ -1,13 +1,8 @@
 extends RigidBody2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	var candy_timer = Timer.new()
 	candy_timer.set_wait_time(2.5)
 	self.add_child(candy_timer)
@@ -15,13 +10,3 @@ func _ready():
 	yield(candy_timer, "timeout")
 	queue_free()
 
-func _physics_process(delta):
-	set_contact_monitor(true)
-	var bodies = get_colliding_bodies()
-	for body in bodies:
-		if body.name == "house_poly":
-			print("turd")
-			self.queue_free()
-		if body.name == "house_wall":
-			print("butt")
-			self.queue_free()
